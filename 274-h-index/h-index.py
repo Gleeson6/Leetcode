1af8e9e1db3afbde929ec52ebf1c87116472d520
Gleeson6 <gleesonminoy28@gmail.com>
2026-02-17 16:19:09 +0530

    Time: 1 ms (32.06%) | Memory: 19.4 MB (48.14%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,9 +1,10 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
+        h=0
         for i in range(len(citations)):
             if citations[i] >= i+1:
-                pass
+                h=h+1
             else:
                 return i
         return len(citations)
