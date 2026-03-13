9238be4f7875527de0ef27bec3e19bb7dc93be66
Gleeson6 <gleesonminoy28@gmail.com>
2026-03-13 13:29:10 +0530

    Time: 397 ms (15.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,19 @@
+SELECT 
+    visited_on,
+    SUM(amount) OVER (
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(
+        AVG(amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ), 2
+    ) AS average_amount
+FROM (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+) AS daily
+ORDER BY visited_on
+LIMIT 100000 OFFSET 6;
